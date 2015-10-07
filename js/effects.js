e1d0adf6b81e8e30c896fa87a911ec56efdb2b52
Bluwaterdogz <bveedy@gmail.com>
2015-10-06 22:48:41 -0700

    update splash page

---
+++ js/effects.js
@@ -0,0 +1,36 @@
+// $(document).onload
+$(document).ready(function(){
+
+		$(".svg3").hide();
+
+ 		$(".svg1").animate({
+			left: '-300px',
+			top: '-90px'
+			}, 3000, function() {
+
+				$(".svg1").animate({
+					left: '-300px',
+					top: '-30px'
+					}, 900, function() {
+
+						$(".svg3").fadeIn(2000);
+						$(".s3p").each(function (i) {
+						    // store the item around for use in the 'timeout' function
+						    var $item = $(this); 
+						    // execute this function sometime later:
+						    setTimeout(function() { 
+						      $item.attr("class",'letteranimate');
+						    }, 200*i);
+						    // each element should animate 200ms after the last one.
+					 	});
+						
+					 			
+					
+				});
+			
+		});
+
+
+
+});
+
