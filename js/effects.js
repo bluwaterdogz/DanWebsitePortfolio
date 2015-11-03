a54313827ce6efd1dfbba3d3fdabcb0af192941b
Bluwaterdogz <bveedy@gmail.com>
2015-11-02 22:13:57 -0800

    prep for merge

--- js/effects.js
+++ js/effects.js
@@ -1,26 +1,32 @@
 $(window).load(function(){
 	window.jQuery || alert("Please enable the Javascripts for your viewing pleasure");
-
-	$(".svg1").animate({
-	left: '-300px',
-	top: '-90px'
-	}, 3000, function() {
-
+	if($(window).width()> 900) {
 		$(".svg1").animate({
-			left: '-300px',
-			top: '-30px'
-			}, 900, function() {
+		left: '-300px',
+		top: '-90px'
+		}, 3000, function() {
+			$(".svg1").animate({
+				left: '-300px',
+				top: '-30px'
+				}, 900, function() {
 
-				$(".svg3").fadeIn(2000);
-				$(".s3p").each(function (i) {
-				    // store the item around for use in the 'timeout' function
-				    var $item = $(this); 
-				    // execute this function sometime later:
-				    setTimeout(function() { 
-				      $item.attr("class",'letteranimate');
-				    }, 200*i);
-				    // each element should animate 200ms after the last one.
-			 	});
-		});
+					$(".svg3").fadeIn(2000);
+					$(".s3p").each(function (i) {
+					    // store the item around for use in the 'timeout' function
+					    var $item = $(this);
+					    // execute this function sometime later:
+					    setTimeout(function() {
+					      $item.attr("class",'letteranimate');
+					    }, 200*i);
+					    // each element should animate 200ms after the last one.
+				 	});
+			});
 	});
+	}else{
+		$(".svg1").animate({
+			left: '20px',
+			top: '20px'
+		},700);
+		$('.cornerMandala').hide();
+	}
 });
